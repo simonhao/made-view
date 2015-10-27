a0a06ae12c0708109012f855b72b651886d1f476
simonhao <simonhao@tencent.com>
2015-10-27 15:03:15 +0800

    修改模块实例计算方法 修改测试案例

--- test/index.js
+++ test/index.js
@@ -6,31 +6,13 @@
 
 'use strict';
 
-//var characterParser = require('character-parser');
 
-//var state = characterParser.parseMax('foo="(", bar=}"}", console.log(qq); var a = 123;} bing bong');
-
-//console.log(characterParser.parseUntil(",",','));
-
-/*var fs        = require('fs');
-var JadeLexer = require('jade-lexer').Lexer;
-
-var filename = '/Users/IetnHao/Projects/MJade/test/layout.jade';
-var str      = fs.readFileSync(filename, 'utf-8');
-
-var tokens = new JadeLexer(str, filename);
-var len = 0;
-while(len < 100){
-  tokens.advance();
-  console.log(tokens.tokens[tokens.tokens.length - 1]);
-  len++;
-}*/
-
-var fs = require('fs');
-var Lexer = require('../lib/lexer.js');
-var Parser = require('../lib/parser.js');
+var Lexer    = require('../lib/lexer.js');
+var Parser   = require('../lib/parser.js');
 var Compiler = require('../lib/compiler.js');
+
 var filename = __dirname + '/extends.jade';
+var fs       = require('fs');
 var str      = fs.readFileSync(filename, 'utf-8');
 
 var Made = require('../index.js');
@@ -39,43 +21,9 @@ var render = Made.compile(str, {
   filename: filename,
   basedir: __dirname,
   entry: 'view.jade',
-  instance: 'top'
+  instance: 'index'
 });
 
 console.log(render({
-  title: '这是输入的值'
+  title: 'Test Page'
 }));
-
-/*var render = Made.compile_file(filename, {
-  basedir: '/Users/IetnHao/Projects/Made-View/test/',
-  entry: 'view.jade',
-  instance: 'top'
-});
-
-console.log(render({
-  person: {},
-  title: [1,2,3],
-  n:0
-}));*/
-
-//var lexer = new Lexer(str, filename);
-//var parser = new Parser(str, filename);
-/*var compiler = new Compiler(str, {
-  filename: filename,
-  basedir: '/Users/IetnHao/Projects/Made-View/test/',
-  entry: 'view.jade',
-  instance: 'top'
-});*/
-//console.log(JSON.stringify(lexer.get_tokens()));
-//console.log(lexer.get_tokens());
-//console.log(JSON.stringify(parser.parse()));
-//console.log(compiler.compile());
-/*var a = 0;
-while(a < 300){
-  lexer.next();
-  a++;
-}
-
-lexer.tokens.forEach(function(token){
-  console.log(token);
-});*/
